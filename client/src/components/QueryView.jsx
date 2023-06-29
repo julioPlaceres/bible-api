62ffbd6712d5c596e19a05931f55b69791dabc5c
dreamheart <leondreamheart@outlook.com>
2023-06-29 19:19:43 -0400

    Reworked QueryView for scalability

--- client/src/components/QueryView.jsx
+++ client/src/components/QueryView.jsx
@@ -11,47 +11,25 @@ import {
 import "./QueryView.css";
 import { useState } from "react";
 
-const renderSwitch = (params) => {
-  switch (params) {
-    case "QUERY_ALL_BOOKS":
-      return <GetAllBooks />;
-
-    case "QUERY_ALL_CHARACTERS":
-      return <GetAllCharacters />;
-
-    case "QUERY_ALL_EVENTS":
-      return <GetAllEvents />;
-
-    case "QUERY_ALL_MATERIALS":
-      return <GetAllMaterials />;
-
-    case "QUERY_ALL_PLACES":
-      return <GetAllPlaces />;
-
-    case "QUERY_ALL_RIVERS":
-      return <GetAllRivers />;
-
-    default:
-      return <h5 className="cw-dft-method">Select an option from the Method dropdown</h5>;
-  }
+const queryComponents = {
+  QUERY_ALL_BOOKS: GetAllBooks,
+  QUERY_ALL_CHARACTERS: GetAllCharacters,
+  QUERY_ALL_EVENTS: GetAllEvents,
+  QUERY_ALL_MATERIALS: GetAllMaterials,
+  QUERY_ALL_PLACES: GetAllPlaces,
+  QUERY_ALL_RIVERS: GetAllRivers,
 };
 
 export const QueryView = () => {
   const [method, setMethod] = useState("");
-  const queryNames = [
-    "QUERY_ALL_BOOKS",
-    "QUERY_ALL_CHARACTERS",
-    "QUERY_ALL_EVENTS",
-    "QUERY_ALL_MATERIALS",
-    "QUERY_ALL_PLACES",
-    "QUERY_ALL_RIVERS",
-  ];
+
   const handleSearch = (event) => {
     event.preventDefault();
 
-    const methodEl = document.getElementById("methodId");
-    setMethod(methodEl.value);
+    setMethod(event.target.value);
   };
+
+  const QueryComponent = queryComponents[method];
 
   return (
     <div className="qv-col">
@@ -60,13 +38,21 @@ export const QueryView = () => {
           id="methodId"
           label="Select a Method"
           name="methodSelect"
-          values={queryNames}
+          values={Object.keys(queryComponents)}
           onChange={handleSearch}
         />
         <Button label="Search" onClick={handleSearch} />
       </div>
 
-      <div className="qv-result-body">{renderSwitch(method)}</div>
+      <div className="qv-result-body">
+        {QueryComponent ? (
+          <QueryComponent />
+        ) : (
+          <h5 className="cw-dft-method">
+            Select an option from the Method dropdown
+          </h5>
+        )}
+      </div>
     </div>
   );
 };
